8e2a53e8eb5deda305b22fbd1042abc6f2b9adfa
ThiagoJuanDEV <tpydeveloper@gmail.com>
2021-04-08 20:01:31 -0300

    Começando a Construção de novas PAges

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <h1>This is the Home Page!</h1>;
+import styles from "../styles/Landing/Landing.module.css";
+
+export default function Landing() {
+  return (
+    <div className={`bg-dark ${styles.landingContainer}`}>
+      <main className={`${styles.main}`}>
+        <p className={styles.mainTitle}>
+          <span className="purple">function</span> study() {"{"}
+          <br />
+          <span className="green">return</span>
+          <span className="green"> flashcards</span>;
+          <br />
+          <span>{"}"}</span>
+        </p>
+
+        <strong>Mude sua maneira de estudar...</strong>
+
+        <div className={styles.buttons}>
+          <div>
+            <button>Registre-se -{">"} </button>
+            <p>
+              Já tem uma conta? faça o <span>login</span>
+            </p>
+          </div>
+          <button>Saiba mais |{">"}</button>
+        </div>
+      </main>
+    </div>
+  );
 }
