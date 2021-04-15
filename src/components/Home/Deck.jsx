cfaefee94b67fcc0d0af368c0b487e10215b2fe7
ThiagoJuanDEV <tpydeveloper@gmail.com>
2021-04-15 13:16:00 -0300

    adicionando funcionalidades

--- src/components/Home/Deck.jsx
+++ src/components/Home/Deck.jsx
@@ -1,11 +1,19 @@
-export default function Deck({ name, description, deck_id, activateModal }) {
+export default function Deck({
+  name,
+  description,
+  deck_id,
+  activateModal,
+  review_info,
+}) {
   return (
     <div
       className="deck"
       style={{ cursor: "pointer" }}
-      onClick={() => activateModal("DeckModal", name, description, deck_id)}
+      onClick={() =>
+        activateModal("DeckModal", name, description, deck_id, review_info)
+      }
     >
-      <span>37</span>
+      <span>20</span>
       <img src="/react.png" alt="react-icon-deck" />
       <strong>{name}</strong>
     </div>
